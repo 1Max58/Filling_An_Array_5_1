0be7d266ef2b5d343c22e70a56772af32ebbba9f
1Max58 <>
2023-11-08 17:59:47 +0200

    Filling and displaying an array implemented

--- src/com/example/fillinganarray/FillingAnArray.java
+++ src/com/example/fillinganarray/FillingAnArray.java
@@ -2,6 +2,14 @@ package com.example.fillinganarray;
 
 public class FillingAnArray {
     public static void main(String[] args) {
-
+        System.out.println("This program creates a 10-element array and puts the numbers from 9 to 0 in it.");
+        int[] nums = new int[10];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = 9 - i;
+        }
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
